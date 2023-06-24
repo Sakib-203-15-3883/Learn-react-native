7ebde94779809135681ca065be64e83826fa43d9
Sakib all hasan <sakibul15.3883@gmail.com>
2023-06-24 10:46:17 +0600

    Debugging in VSCode

--- App.js
+++ App.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 //Any component that renders other components is a parent component. Here, App is the parent component and Calculator is a child component.
 
-// import Calculator from './components/calculator';
-import Props from './components/Props';
+import Calculator from './components/calculator';
+// import Props from './components/Props';
 
-const App = () => {
-  const name = 'John Doe';
-  const age = 30;
-  const hobbies = ['Reading', 'Gardening', 'Cooking'];
+ const App = () => {
+//   const name = 'John Doe';
+//   const age = 30;
+//   const hobbies = ['Reading', 'Gardening', 'Cooking'];
 
-  return <Props name={name} age={age} hobbies={hobbies} />;
+  return <Calculator/>
+  
 
 };
 
